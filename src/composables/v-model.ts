949b280a2844f8e0f9b3ec0985492ae2469028cf
shareef <creazyhell@gmail.com>
2023-09-18 13:35:08 +0200

    remove unused generic

--- src/composables/v-model.ts
+++ src/composables/v-model.ts
@@ -1,16 +1,17 @@
+import { computed, getCurrentInstance } from 'vue'
 import type { Getter } from '@/types'
-import { getCurrentInstance, computed } from 'vue'
 
 interface UseVModelOptions<T> {
   setter?: (newValue: T) => T
   eventName?: string
 }
 
-export function useVModel<T, U = T>(getter: Getter<T>, options: UseVModelOptions<T> = {}) {
+export function useVModel<T>(getter: Getter<T>, options: UseVModelOptions<T> = {}) {
   const { eventName = 'update:modelValue', setter } = options
   const vm = getCurrentInstance()
 
   const emit = vm?.emit || (vm as any)?.$emit?.bind(vm) || vm?.proxy?.$emit?.bind(vm?.proxy)
+
   if (!emit) {
     throw new Error('[vex] `useVModel` Cannot find emit function on Vue instance.')
   }
