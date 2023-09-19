fdf4725e31d2c4509579072fb77ed7be020e6777
shareef <creazyhell@gmail.com>
2023-09-19 07:35:43 +0200

    add production config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { URL, fileURLToPath } from 'node:url'
+import { resolve } from 'path'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index'),
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
 })
