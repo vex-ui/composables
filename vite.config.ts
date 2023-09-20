699d3c8430e86af620490486ad07bdbda86721d2
shareef <creazyhell@gmail.com>
2023-09-20 08:23:03 +0200

    added plugin dts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { resolve } from 'path'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
