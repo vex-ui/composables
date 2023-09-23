7ab7cf02d216a4057b365e2edb0edaf6276abc26
shareef <creazyhell@gmail.com>
2023-09-23 07:14:06 +0200

    use direction and orientation aware code

--- src/list-highlight.ts
+++ src/list-highlight.ts
@@ -1,49 +1,43 @@
-import { useEventListener } from '.'
-import { type Ref, watch } from 'vue'
-import { isFunction } from '@/utils'
+import type { Fn, Getter, Orientation, TemplateRef } from '@/types'
+import type { Ref } from 'vue'
+import { useKeyIntent } from '.'
+
+interface Options {
+  orientation?: Getter<Orientation>
+}
 
 export function useListHighlight(
-  parent: Ref<HTMLElement | null>,
+  parent: TemplateRef,
   highlighted: Ref<number>,
-  children: Ref<HTMLElement[]>
-) {
-  useEventListener(parent, 'keydown', (e: KeyboardEvent) => {
-    if (!['ArrowDown', 'ArrowUp', 'Home', 'End'].includes(e.key)) return
+  children: Ref<HTMLElement[]>,
+  options: Options = {}
+): Fn {
+  return useKeyIntent(
+    parent,
+    (e: KeyboardEvent, intent) => {
+      const last = children.value.length - 1
+      const oldValue = highlighted.value
+      e.preventDefault()
+      e.stopPropagation()
 
-    const last = children.value.length - 1
-    e.preventDefault()
-    e.stopPropagation()
+      switch (intent) {
+        case 'next':
+          highlighted.value = oldValue >= last ? 0 : oldValue + 1
+          break
 
-    switch (e.key) {
-      case 'ArrowDown':
-        setHighlighted((v) => (v >= last ? 0 : v + 1))
-        break
+        case 'prev':
+          highlighted.value = oldValue <= 0 ? last : oldValue - 1
+          break
 
-      case 'ArrowUp':
-        setHighlighted((v) => (v <= 0 ? last : v - 1))
-        break
+        case 'first':
+          highlighted.value = 0
+          break
 
-      case 'Home':
-        setHighlighted(0)
-        break
-
-      case 'End':
-        setHighlighted(last)
-        break
-    }
-  })
-
-  watch(
-    highlighted,
-    (curr: number, prev: number) => {
-      const items = children.value
-      items[prev]?.classList.remove('--highlighted')
-      items[curr]?.classList.add('--highlighted')
+        case 'last':
+          highlighted.value = last
+          break
+      }
     },
-    { flush: 'sync' }
+    options
   )
-
-  function setHighlighted(setter: ((v: number) => number) | number): void {
-    highlighted.value = isFunction(setter) ? setter(highlighted.value) : setter
-  }
 }
